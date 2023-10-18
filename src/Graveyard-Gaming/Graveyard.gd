eb24468ac57a5bbb8da669171d24894b621422af
Flarglfump <witskeng@yahoo.com>
2023-10-17 22:22:34 -0500

    Exported Windows Build

--- src/Graveyard-Gaming/Graveyard.gd
+++ src/Graveyard-Gaming/Graveyard.gd
@@ -26,7 +26,6 @@ func retrieve_wave_data():
 func spawn_enemies(wave_data):
 	for i in wave_data:
 		print("Spawning " + str(i[0]))
-		var enemy: PackedScene
 		var new_enemy = load("res://Enemies/" + str(i[0]) + ".tscn").instantiate()
 		var new_pos = Vector2(right_border, 100 * (randi()%5) + top_offset)
 		print("setting position to " + str(new_pos.x) + ", " + str(new_pos.y))
